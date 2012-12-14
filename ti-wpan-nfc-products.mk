ae3c8888b720830f9c553664cb1d6be7e960db1b
Manjunatha Halli <x0130808@ti.com>
2012-12-14 11:30:06 -0600

    ti-wpan-nfc-products.mk: Adding the product packages required for NFC
    Signed-off-by: Manjunatha Halli <x0130808@ti.com>
    Signed-off-by: Michael Bestas <mikeioannina@gmail.com>
    
    Change-Id: If9d153530bfa4b4365c136bfb01e05990b5a9645

---
+++ ti-wpan-nfc-products.mk
@@ -0,0 +1,14 @@
+# This file lists the software specific to WiLink connectivity chip
+# NFC
+PRODUCT_PACKAGES += \
+    libnfc \
+    libnfc_ndef \
+    libnfc_jni \
+    Nfc \
+    NFCDemo \
+    Tag \
+    TagTests \
+    TagCanon \
+    AndroidBeamDemo \
+    NfcExtrasTests \
+    com.android.nfc_extras
--- ti-wpan-products.mk
+++ ti-wpan-products.mk
@@ -1,6 +1,5 @@
-# This file lists the firmware, software that are specific to
-# WiLink connectivity chip on OMAPx platforms.
-
+# This file lists the software specific to WiLink connectivity chip.
+# BT
 PRODUCT_PACKAGES += \
     uim-sysfs \
     libbt-vendor
