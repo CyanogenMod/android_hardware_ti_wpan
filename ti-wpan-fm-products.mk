50789736ee4f3a5d99a6f2d3baabde57aff0799b
Michael Bestas <mikeioannina@gmail.com>
2013-10-31 06:46:11 +0200

    ti-wpan-fm-products.mk: Adding the product packages required for FM
    Change-Id: I4d894e4d8c199012ab3b40cc9087d47d809b08be
    (cherry picked from commit bbad388c06a0045df7f3b1bc36c6f1970e40da5a)

---
+++ ti-wpan-fm-products.mk
@@ -0,0 +1,9 @@
+# This file lists the software specific to WiLink connectivity chip
+# FM
+PRODUCT_PACKAGES += \
+    kfmapp \
+    FmRxApp \
+    FmService \
+    libfmradio \
+    fmradioif \
+    com.ti.fm.fmradioif.xml
